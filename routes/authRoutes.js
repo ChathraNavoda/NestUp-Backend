ff789ad45869ffbd17c4d4b1c45d209b6581ee88
ChathraNavoda <chathra96@gmail.com>
2025-09-30 11:49:38 +0530

    feat(auth): add GET /auth/me route and JWT token support

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, loginUser, getMe } = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authMiddleware, getMe); // <-- add this
 
 module.exports = router;
